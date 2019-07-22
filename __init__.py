3a9fb1a70921e1eb0e3fd0bd0404c6940dd292ec
Thomas Axelsson <thomasa88@gmail.com>
2019-07-22 18:42:19 +0200

    License information and copyright

--- __init__.py
+++ __init__.py
@@ -1,3 +1,23 @@
+#
+# Copyright 2019 Thomas Axelsson <thomasa88@gmail.com>
+#
+# This file is part of pyets2_telemetry_server.
+#
+# pyets2_telemetry_server is free software: you can redistribute it
+# and/or modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation, either version 3 of
+# the License, or (at your option) any later version.
+#
+# pyets2_telemetry_server is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with pyets2_telemetry_server.
+# If not, see <https://www.gnu.org/licenses/>.
+#
+
 import logging
 import math
 import threading
--- web_server.py
+++ web_server.py
@@ -1,4 +1,22 @@
-#!/usr/bin/env python3
+#
+# Copyright 2019 Thomas Axelsson <thomasa88@gmail.com>
+#
+# This file is part of pyets2_telemetry_server.
+#
+# pyets2_telemetry_server is free software: you can redistribute it
+# and/or modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation, either version 3 of
+# the License, or (at your option) any later version.
+#
+# pyets2_telemetry_server is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with pyets2_telemetry_server.
+# If not, see <https://www.gnu.org/licenses/>.
+#
 
 # SignalR protocol info:
 # https://blog.3d-logic.com/2015/03/29/signalr-on-the-wire-an-informal-description-of-the-signalr-protocol/
